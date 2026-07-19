9871797e066374648d104e4420816d5814453b91
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-19 16:52:36 +1000

    Setup runtime config.

--- auth/nitro.config.ts
+++ auth/nitro.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
     routes: {
         "/auth/**": "./server/api/**"
     },
+    
+    devProxy: {
+        "/auth/**": {
+            target: `http://${process.env.HOST_ADDRESS || "localhost"}:${process.env.SERVICE_PORT || 4000}`,
+            changeOrigin: true
+        }
+    },
     routeRules: {
         "/auth/**": {
             cors: true,
@@ -13,6 +20,15 @@ export default defineConfig({
                 "Access-Control-Allow-Methods": "POST, OPTIONS",
                 "Access-Control-Allow-Headers": "Content-Type, Authorization"
             }
+        }
+    },
+    runtimeConfig: {
+        jweSecret: process.env.JWE_SECRET || 'your-secret-key',
+        public: {
+            redisHost: process.env.REDIS_HOST || 'localhost',
+            redisPort: process.env.REDIS_PORT || 6379,
+            service_port: process.env.SERVICE_PORT || 4000,
+            hostname: process.env.HOST_ADDRESS || "localhost",   
         }
     }
 });
