361b694614e25fa94ef2e95429346ccd8f0a5627
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-19 16:02:09 +1000

    Modularise password helper function.

--- auth/helper/password.ts
+++ auth/helper/password.ts
@@ -1,11 +1,18 @@
 import bcrypt from 'bcryptjs';
 
-export function hashPassword(password: string): string {
+function hashPassword(password: string): string {
     // Implement your password hashing logic here
     return bcrypt.hashSync(password, 12); 
 }
 
-export function verifyPassword(password: string, hashedPassword: string): boolean {
+function verifyPassword(password: string, hashedPassword: string): boolean {
     // Implement your password verification logic here
     return bcrypt.compareSync(password, hashedPassword); 
 }
+
+const passwordHelper = {
+    hashPassword,
+    verifyPassword
+};
+
+export default passwordHelper;
