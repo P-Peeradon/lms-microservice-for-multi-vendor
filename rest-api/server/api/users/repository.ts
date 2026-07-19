fe849a1a14e9b5b9c73e7dee88f29a960174d823
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-20 01:26:33 +1000

    Implement repository.

--- rest-api/server/api/users/repository.ts
+++ rest-api/server/api/users/repository.ts
@@ -1,11 +1,13 @@
+import pool from "../../../../database/mysql";
+
 class UserRepository {
 
     static async create(userData: any) {
         const query = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
         const values = [userData.name, userData.email, userData.password];
 
-        const result = await useQuery(query, values);
-        return { id: result.insertId, ...userData };
+        const result = await pool.execute(query, values);
+        return { id: result[0].insertId, ...userData };
     }
 
 }
