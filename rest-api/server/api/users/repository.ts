0e84e49ceb652efe0a0fd2a074435354e91f5152
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-20 14:33:27 +1000

    Correct the schema.

--- rest-api/server/api/users/repository.ts
+++ rest-api/server/api/users/repository.ts
@@ -1,11 +1,30 @@
 import pool from "../../../../database/mysql";
 import type { RowDataPacket } from "mysql2/promise";
 
+/*
+CREATE TABLE IF NOT EXISTS lms_lumiere_carnet.users (
+	shadow_id CHAR(9),
+    encrypted_pii BLOB,
+    university VARCHAR(100) NOT NULL,
+    faculty VARCHAR(80) NOT NULL,
+    firstname BINARY(32) NOT NULL,
+    uni_id BINARY(32) UNIQUE NOT NULL,
+    date_of_birth BINARY(32) NOT NULL,
+    PRIMARY KEY (shadow_id)
+);
+*/
+
 class UserRepository {
 
     static async create(userData: any) {
-        const query = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
-        const values = [userData.name, userData.email, userData.password];
+        const query = `
+            INSERT INTO users (shadow_id, encrypted_pii, university, faculty, firstname, uni_id, date_of_birth) 
+            VALUES (?, ?, ?, ?, ?, ?, ?);
+        `;
+        const values = [userData.shadow_id, userData.pii, 
+            userData.university, userData.faculty, 
+            userData.firstname, userData.uni_id, 
+            userData.date_of_birth];
 
         const result = await pool.execute<RowDataPacket[]>(query, values);
         return { id: result[0].insertId, ...userData };
