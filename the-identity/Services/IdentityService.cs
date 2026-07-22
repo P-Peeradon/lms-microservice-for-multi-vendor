eba099177ee5e89937150f18f56aa400b86e7c0d
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-22 20:18:06 +1000

    Implement route correctly.

--- the-identity/Services/IdentityService.cs
+++ the-identity/Services/IdentityService.cs
@@ -6,11 +6,21 @@ namespace the_identity.Services;
 public class IdentityService : Identity.IdentityBase
 {
 
+    private readonly ILogger<IdentityService> _logger;
+
+    public IdentityService(ILogger<IdentityService> logger)
+    {
+        _logger = logger;
+    }
+
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
+
+        _logger.LogInformation("[C# Identity] Dynamic call invoked for: {Name}", request.Name);
+
         return Task.FromResult(new HelloReply
         {
-            Message = "Hello " + request.Name
+            Message = $"Hello {request.Name}, greetings dynamically from C# Identity microservice!"
         });
     }
 }
