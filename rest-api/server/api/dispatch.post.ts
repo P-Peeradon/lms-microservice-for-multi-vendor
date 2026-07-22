5e29456a7b77f94c1c3364dc521d128f0df599d1
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-22 20:34:30 +1000

    Dispatch event.

---
+++ rest-api/server/api/dispatch.post.ts
@@ -0,0 +1,48 @@
+import busClient from '~/helper/grpcClient';
+import { readBody, defineEventHandler, type H3Event, HTTPError } from 'h3';
+
+interface GrpcRequest {
+    target_service: string; // e.g., "order_service", "payment_service"
+    event_name: string;     // e.g., "CreateOrder", "ProcessPayment"
+    payload: object | string;        // JSON or serialized data
+}
+
+// Promisify gRPC call for async/await in Nitro
+const sendToBus = (requestData: GrpcRequest): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        busClient.ProcessEvent(requestData, (err: any, response: any) => {
+            if (err) return reject(err);
+            resolve(response);
+        });
+    });
+};
+
+export default defineEventHandler(async (event: H3Event) => {
+    const body: GrpcRequest | undefined = await readBody(event);
+
+    if (!body || !body.target_service || !body.event_name) {
+        throw new HTTPError({
+            statusCode: 400, 
+            statusMessage: 'Invalid request body. Required fields: target_service, event_name'
+        });
+    }
+
+    try {
+        
+        const grpcResponse = await sendToBus({
+            target_service: body.target_service,
+            event_name: body.event_name,
+            payload: body.payload ? JSON.stringify(body.payload) : {},
+        });
+
+        return {
+            status: 'success',
+            data: grpcResponse,
+        };
+    } catch (err: any) {
+        throw new HTTPError({
+            statusCode: 502, 
+            statusMessage: 'Failed to communicate with Python Bus'
+        });
+    }
+});
