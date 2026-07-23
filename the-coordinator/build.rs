0e256d22643346419576f6134e939f955e9762b6
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-07-23 17:36:30 +1000

    Implement building schema

--- the-coordinator/build.rs
+++ the-coordinator/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("Protos/coordinator.proto")?;
+    tonic_prost_build::compile_protos("protos/coordinator.proto")?;
     Ok(())
 }
